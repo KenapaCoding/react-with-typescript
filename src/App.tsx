dfcc72d197ab0cdcdd39b6595b91dec1888be68a
ranggo pato <patoranggo@gmail.com>
2024-10-18 07:03:47 +0700

    aturan-jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-/** @format */
-
 import './App.css';
 
 function App() {
-  const nama:string = 'Ranggo'
+	const nama: string = 'Ranggo';
 	return (
 		<>
 			<h1 className='red-text'>Hello {nama} !</h1>
 			<p>ini description</p>
-      <input type="text" />
-      <img src="" alt="" />
+			<input type='text' />
+			<img
+				src=''
+				alt=''
+			/>
 		</>
 	);
 }
